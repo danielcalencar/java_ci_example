8047e2876586a8f5ef5a20b942451efa18355bd5
danielcalencar <daniel.calencar@gmail.com>
2024-04-18 11:14:55 +1200

    Update App.java
    making my code well documented

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App {
         return "Hello World!";
     }
 
+	//adding documentation
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
